bfa99213c4d2afdc4249b44feac63f1b4856f6ff
Dylan Cochran <heliocentric@gmail.com>
2016-06-20 14:42:17 -0400

    Add fix for ruby 1.9 rvm

--- provision.pp
+++ provision.pp
@@ -14,7 +14,6 @@ rvm_system_ruby {
   'ruby-1.9':
     ensure      => 'present',
     default_use => true,
-    build_opts  => ['--binary'];
   'ruby-2.0':
     ensure      => 'present',
     default_use => false;
