4cfca74477dbdafe41fcc096d0a6c8b94e08bca1
Dylan Cochran <heliocentric@gmail.com>
2016-06-22 10:22:27 -0400

    Fix gemfile for the vagrant box.

--- provision.pp
+++ provision.pp
@@ -1,4 +1,5 @@
 $packages = [
+   "git",
    "nano",
 ]
 package { $packages:
@@ -11,14 +12,27 @@ file { "/vagrant/simp-core/src/doc":
 class { "::rvm": }
 
 rvm_system_ruby {
-  'ruby-1.9':
+  'ruby-1.9.3':
     ensure      => 'present',
-    default_use => true,
-  'ruby-2.0':
+    default_use => true;
+  'ruby-2.0.0':
     ensure      => 'present',
     default_use => false;
   'ruby-2.2.5':
     ensure      => 'present',
     default_use => false;
 
+}
+rvm_gem {
+  'ruby-2.0.0/bundler':
+    ensure  => latest;
+  'ruby-1.9.3/bundler':
+    ensure  => latest;
+  'ruby-2.2.5/bundler':
+    ensure  => latest;
+}
+file_line { "changegemfile":
+  path => "/vagrant/simp-doc/Gemfile",
+  match => "gem 'puppet-strings', :git => 'https://github.com/puppetlabs/puppetlabs-strings.git'",
+  line => "gem 'puppet-strings', :git => '/vagrant/modules/strings'",
 }
